03db8c8e5a94bde4248d360f1b6454a62b67cb6e
Morgan McEntire <mmcenti@g.clemson.edu>
2016-04-26 16:09:45 -0400

    monitor mode

--- rollcall.h
+++ rollcall.h
@@ -19,41 +19,41 @@
 #define def_password "c0b60c6a" /* password (default = none) */
 #define def_db_name "heroku_89678d1e4934514" /* database to use (default = none) */
 #define COMMAND_SIZE 73
+#define SIZE_ETHERNET 14
+#define	T_DATA 0x2
+#define	ETHERTYPE_IP		0x0800	/* IP protocol */
+#define ETHERTYPE_IPV6		0x86dd	/* IPv6 */
+#define ip6_nxt   ip6_ctlun.ip6_un1.ip6_un1_nxt
+#define FC_TYPE(fc) (((fc) >> 2) & 0x3)
+#define FC_SUBTYPE(fc) (((fc) >> 4) & 0xF)
+#define FC_TO_DS(fc) ((fc) & 0x0100)
+#define FC_FROM_DS(fc) ((fc) & 0x0200)
+#define DATA_FRAME_IS_QOS(x) ((x) & 0x08)
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+
+typedef u_int tcp_seq;
 
 /***************************************
  *                                     * 
  *   Structs to represent headers      *
  *                                     *
  ***************************************/
-typedef struct ethernet_h_tag{
-    const struct ether_addr ether_dest_host; //the destination host address
-    const struct ether_addr ether_src_host; //the source host address
-    u_short ether_type; //to check if its ip etc
-}ethernet_h;
+typedef struct radiotap_header {
+    u_int8_t	it_version;	/* set to 0 */
+    u_int8_t	it_pad;
+    u_int16_t	it_len;		/* entire length */
+    u_int32_t	it_present;	/* fields present */
+} radiotap_h;
+typedef struct wifi_header {
+    u_int16_t fc;
+    u_int16_t duration;
+    const struct ether_addr dst_addr;
+    const struct ether_addr src_addr;
+    u_int8_t bssid[6];
+    u_int16_t seq_ctrl;
+} wifi_h;
 
-typedef struct ip_h_tag{
-    unsigned char ip_vhl; //assuming its ipv4 and header length more than 2
-    unsigned char service; //type of service
-    unsigned short total_len; //total length
-    unsigned short identification; // identification
-    u_short ip_off; //offset field
-    u_char ttl; // time to live value
-    u_char ip_protocol; // the protocol
-    u_short sum; //the checksum
-    struct in_addr ip_src;
-    struct in_addr ip_dst;
-
-    #define IP_HL(ip)       (((ip)->ip_vhl) & 0x0f)
-    #define IP_V(ip)        (((ip)->ip_vhl) >> 4)
-}ip_h;
-
-
-typedef struct tcp_h_tag{
-    u_short src_port;   /* source port */
-    u_short dst_port;   /* destination port */
-}tcp_h;
-
-void print_packet(const u_char*, int, ethernet_h*, ip_h*, tcp_h*);
+void print_packet(const u_char*, radiotap_h*, wifi_h*);
 void getCommandLine(int, char**, char**, char*);
 void accessDatabase();
 void process_result_set(MYSQL *conn, MYSQL_RES *res_set, char *macs[]);

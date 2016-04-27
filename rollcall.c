12e9348cfd7aab09a0390448a3f5c90f32541ca5
Morgan McEntire <mmcenti@g.clemson.edu>
2016-04-27 10:28:10 -0400

    Fix comments

--- rollcall.c
+++ rollcall.c
@@ -1,3 +1,13 @@
+/*
+* Program: rollcall.c
+* Name: Isiah Hamilton, Ben Hindman, Joshua Johnston, Morgan McEntire
+* Date: 4/27/2016
+* Purpose: Scans eduroam for available channels and then searches for MAC addresses
+* across all channels and logs missing MACs to log file
+* Assumptions: OS X ONLY, MAC addresses are in DB, rollcall.h has DB connection info,
+* uses mysql c connector
+*/
+
 #include "rollcall.h"
 
 int main(int argc, char *argv[]){
@@ -12,20 +22,20 @@ int main(int argc, char *argv[]){
     int *channels;
     FILE *fp = fopen("absences.log","w");
     
-    const char s1[] = "wlan src ";
+    const char s1[] = "wlan src ";      //used in pcap filter
 
     /*Header Structs*/
     radiotap_h *radiotap = NULL;
     wifi_h *wifi = NULL;
     
     // GET MAC ADDRESSES 
-    //getMacsDb(addrs);
-
-    addrs[0] = "3c:15:c2:ed:b3:dc";
-    addrs[1] = "f4:5c:89:89:fd:93";
-    addrs[2] = "4d:a5:22:f1:b3:5e";
-    addrs[3] = "f4:f2:60:17:23:e8";
-    addrs[4] = "a4:5e:60:e7:a3:15";
+    getMacsDb(addrs);
+    //hardcoded backup
+    // addrs[0] = "3c:15:c2:ed:b3:dc";
+    // addrs[1] = "f4:5c:89:89:fd:93";
+    // addrs[2] = "5c:c5:d4:58:64:66";
+    // addrs[3] = "f4:f2:60:17:23:e8";
+    // addrs[4] = "a4:5e:60:e7:a3:15";
 
     int num_macs = sizeof(addrs)/sizeof(addrs[0]);
     //printf("Number of macs = %d\n", num_macs);
@@ -33,6 +43,7 @@ int main(int argc, char *argv[]){
         printf("%s\n",addrs[i]);
     }*/
 
+    //Get the number of channels to search and what they are
     int ch_length = 0;
     channels=getChannels(&ch_length);
     
@@ -42,7 +53,6 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
         return(2);
     }
-    //printf("\nDevice: %s\n\n", device);
   
     pcap_lookupnet(device, &net, &mask, errbuf);
 
@@ -55,8 +65,10 @@ int main(int argc, char *argv[]){
         if (i>0) {
             pcap_close(handle);
         }
+        //Change channels to search for MAC address
         change_channel(channels[i]);
         
+        //Recreate handle
         handle = pcap_create(device, errbuf);
         pcap_set_rfmon(handle, 1);
         if (pcap_set_promisc(handle, 1) == PCAP_ERROR_ACTIVATED) /* Capture packets that are not yours */
@@ -68,6 +80,7 @@ int main(int argc, char *argv[]){
         
         for (j = 0; j < num_macs; j++) {
             if(addrs[j] != NULL){
+                //create pcap filter
                 strcpy(filter, s1);
                 strcat(filter, addrs[j]);
                 
@@ -94,6 +107,7 @@ int main(int argc, char *argv[]){
                 //Printing packet information
                 if (packet!=NULL){
                     print_packet(packet, header.len, radiotap, wifi); 
+                    //set addrs[j] to NULL if MAC has been found
                     addrs[j] = NULL;  
                 }
             } 
@@ -111,6 +125,13 @@ int main(int argc, char *argv[]){
     return(0);
 }
 
+/* Purpose: Grab command line functions from the user
+ *
+ * Parameters: argc, argv, variable to store device name, error buffer
+ *
+ * Returns: N/A
+ *
+ */
 void getCommandLine(int argc, char **argv, char **device, char *errbuf){
     int c;
     while ((c = getopt(argc, argv, "d:f")) != -1)
@@ -126,20 +147,26 @@ void getCommandLine(int argc, char **argv, char **device, char *errbuf){
                 exit(1);
         }
 }
-
+/*
+* Purpose: Connect to DB and get mac addresses. Then it stores them
+*          in an array passed by reference
+*
+* Parameters: Array to hold MAC addresses
+*
+* Returns: Theoretically nothing, but it changes the mac_addrs to update
+*          with the MAC addresses from the database by calling process_result_set
+*/
 void getMacsDb(char *mac_addrs[]){
-    char *present[2];
-    char *absent[2];
-    char command[COMMAND_SIZE];
     MYSQL_RES *res_set;
-    int i, j, here;
 
+    //Initialize MySQL connection
     conn = mysql_init (NULL);
     if (conn == NULL){
         fprintf (stderr, "mysql_init() failed (probably out of memory)\n");
         exit (1);
     }
 
+    //Connect to the database
     if (mysql_real_connect (
                 conn, //Pointer to connection handler 
                 def_host_name, //Host to connect to 
@@ -155,6 +182,7 @@ void getMacsDb(char *mac_addrs[]){
         exit (1);
     }
 
+    //Send the query to the database to grab the table
     if (mysql_query (conn, "SELECT * from student") != 0){
         printf("mysql_query() failed\n");
     }else{
@@ -168,14 +196,16 @@ void getMacsDb(char *mac_addrs[]){
         }
     }
 
-    //based on reading packets
-    present[0] = mac_addrs[0];
-    present[1] = mac_addrs[1];
-
     mysql_close (conn);
 }
 
-//get mac addresses from db and store in macs array
+/* Purpose: Get MAC addresses from database and store in macs array
+ *
+ * Parameters: conn, res_set, array for MAC addresses
+ *
+ * Returns: N/A
+ *
+ */
 void process_result_set(MYSQL *conn, MYSQL_RES *res_set, char *macs[]){
     MYSQL_ROW row;
     unsigned int i=0;
@@ -192,6 +222,16 @@ void process_result_set(MYSQL *conn, MYSQL_RES *res_set, char *macs[]){
     }
 }
 
+/*
+*  Purpose: Changes channels on the access point using OS X airport utility
+*           WARNING: will disconnect you from network and
+*           you will need to manually reconnect
+*
+* Parameters: Channel to connect to
+*
+* Returns: N/A
+*
+*/
 
 void change_channel(int new_channel) {
     /*if mac with airport utility*/
@@ -212,6 +252,14 @@ void change_channel(int new_channel) {
     
 }
 
+/* Purpose: Prints out the packet information - the MAC address it found
+ *          and where it was transmitting to.
+ *
+ * Parameters: the packet, the size, the radiotap header, and the wifi header
+ *
+ * Returns: N/A
+ *
+ */
 void print_packet(const u_char *packet, int size, radiotap_h *radiotap, wifi_h *wifi){
     int size_radiotap;
     
@@ -248,6 +296,15 @@ void print_packet(const u_char *packet, int size, radiotap_h *radiotap, wifi_h *
     //printf("-------------------------------\n\n");
 }
 
+/*
+*  Purpose: Grabs the available channels using airport. It stores the channels
+*           in an array and keeps track of how many channels there are.
+*
+*  Parameters: Number of channels (passed by reference to have two returns)
+*
+*  Returns: The array of channels. Also changes count
+*
+*/
 int* getChannels(int *count){
     char path[1035];
     char channel[4];
@@ -267,6 +324,8 @@ int* getChannels(int *count){
     
     printf("Channels: ");
     while (fgets(path, sizeof(path)-1, fp) != NULL) {
+        //these values come from the length of the output string from airport
+        //channel numbers always start at 56 and can be 3 chars long
         channel[0] = path[56];
         channel[1] = path[57];
         channel[2] = path[58];
@@ -276,6 +335,7 @@ int* getChannels(int *count){
         sprintf(full_channel, "%c%c%c",channel[0],channel[1],channel[2]);
         temp = (int) strtol(full_channel, (char **)NULL, 10);
         
+        //Grabs only unique channels
         check = 0;
         i=0;
         while (i < *count) {

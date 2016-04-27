2f98d9bc7736e6c5f0642974ae2c5a9fcc2508a0
Morgan McEntire <mmcenti@g.clemson.edu>
2016-04-27 03:16:51 -0400

    final version

--- submit.c
+++ submit.c
@@ -1,21 +1,50 @@
 #include "rollcall.h"
 
-int main(int argc, char *argv[]){
-    char *mac_addrs[2];
-    int count=0;
+int main(void)
+{
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char command[COMMAND_SIZE];
 
-    //Try to open the file for reading
-    FILE *fp=fopen("absences.log","r");
-    if(fp==NULL){
-        printf("Error. Couldn't open file.\n");
-        exit(1);
+    conn = mysql_init (NULL);
+    if (conn == NULL){
+        fprintf (stderr, "mysql_init() failed (probably out of memory)\n");
+        exit (1);
     }
 
-    //Read entire file
-    while(!feof(fp)){
-
+    if (mysql_real_connect (
+                conn, //Pointer to connection handler 
+                def_host_name, //Host to connect to 
+                def_user_name, //User name 
+                def_password, //Password 
+                def_db_name, //Database to use 
+                0, //Port (use default) 
+                NULL, //Socket (use default) 
+                0) //Flags (none) 
+            == NULL)
+    {
+        fprintf (stderr, "mysql_real_connect() failed:\nError %u (%s)\n", mysql_errno (conn), mysql_error (conn));
+        exit (1);
     }
-    
-    return 0;
+    fp = fopen("absences.log", "r");
+    if (fp == NULL)
+        exit(2);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        line[17] = '\0'; //put null at end of string
+        snprintf(command, COMMAND_SIZE, "UPDATE student SET absences = absences + 1 WHERE mac='%s'", line);
+        if (mysql_query (conn, command) != 0){
+            printf("mysql_query() failed\n");
+        }else{
+            printf("Student marked absent\n");
+        }
+    }
+    fclose(fp);
+    mysql_close (conn);
+
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
 }
-
